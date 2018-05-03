72e2c990c3ff5b57c65e79ac6edb7bf000ce69bb
Esop Baek <esop.baek@gmail.com>
2018-05-03 14:04:15 -0400

    Add create post functionality

---
+++ db/migrate/20180503175318_create_postsrake.rb
@@ -0,0 +1,6 @@
+class CreatePostsrake < ActiveRecord::Migration[5.1]
+  def change
+    create_table :postsrakes do |t|
+    end
+  end
+end
---
+++ spec/features/user_creates_post_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+feature "User creates blog" do
+	scenario "successfully" do
+		visit root_path
+
+		click_on "Add a new post"
+		fill_in "Title", with: "The diversification of state economies"
+		click_on "Submit"
+
+		expect(page).to have_css '.posts li', text: "The diversification of state economies"
+	end
+end
